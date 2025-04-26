459903f8c684887e089427f70c380258ba418194
hezou <hezoubeke18@gmail.com>
2025-04-26 21:30:53 +0000

    update

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,10 +1,8 @@
 import mongoose from 'mongoose';
-
 export const connectDB = async () => {
     try {
-        const conn = mongoose.connect(process.env.MONGODB_URI);
-        console.log('MongoDB connected: ${conn.connection.host}');
-
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (error) {
          console.log("MongoDb connection error :", error);
         
