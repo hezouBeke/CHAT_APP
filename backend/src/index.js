2f339f3d4697ffad77fc2b6bc10c709cdc8d6cc3
hezou <hezoubeke18@gmail.com>
2025-04-28 16:06:25 +0000

    update

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,7 +6,11 @@ import { connectDB } from "./lib/db.js";
 
 const app = express();
 dotenv.config();
+
+app.use(express.json())
 app.use("/api/auth", authRoutes);
+
+
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
