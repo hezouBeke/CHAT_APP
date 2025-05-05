7645d6d355649fd3da45fd3d5e533e3568891409
hezou <hezoubeke18@gmail.com>
2025-05-05 14:23:59 +0000

    update

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js"
+ 
 import { connectDB } from "./lib/db.js";
 
 const app = express();
@@ -10,7 +12,10 @@ dotenv.config();
 
 app.use(express.json())
 app.use(cookieParser())
+
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+
 
 
 
