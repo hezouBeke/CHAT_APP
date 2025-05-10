434abeba8a9ba6afe3c0827b1d9c8554e4724ef1
hezou <hezoubeke18@gmail.com>
2025-05-10 17:07:05 +0000

    update

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,5 @@
 import User from "../models/user.model.js";
-
+import Message from "../models/message.model.js";
 
 export const getUsersForSidebar = async (req, res) =>{
 
@@ -11,8 +11,31 @@ export const getUsersForSidebar = async (req, res) =>{
     
   } catch (error) {
     console.log("error in getUsersForSidebar", error.message);
-    res.status(500).json({ error: "internal server error"});
-    
-    
+    res.status(500).json({ error: "internal server error"}); 
   }
 };
+
+export const getMessages = async (req, res) => {
+    try {
+        const {id:userToChatId} = req.params;
+        const myId= req.user._id;
+        
+        const messages = await Message.find({
+            $or:[
+                {senderId:myId, receiverId:userToChatId},
+                {senderId:userToChatId, receiverId:myId},
+            ],
+        });
+        res.status(200).json(messages);
+
+    } catch (error) {
+        console.log("error in getMessages", error.message);
+        res.status(500).json({ error: "internal server error"}); 
+        
+    }
+
+}
+
+export const sendMessage = async (req, res) =>{
+    
+}
