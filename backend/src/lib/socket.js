cda9e68b85928d549ac7f3e1b8664fc601f3b03d
hezou <hezoubeke18@gmail.com>
2025-05-16 15:42:24 +0000

    update

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -8,17 +8,27 @@ const server= http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:000"],
-    }
-})
+        origin: ["http://localhost:3000"],
+    },
+});
+
+const userSocketMap = {}; 
+
 
 io.on("connection", (socket) => {
     console.log("A user connected",socket.id);
 
 
+    const userId = socket.handshake.query.userId;
+    if (userId) userSocketMap[userId] = socket.id;
+
+      // io.emit() is used to send events to all the connected clients
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
     socket.on("disconnect", () => {
         console.log("A user disconnected", socket.id);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
     })
 })
 
