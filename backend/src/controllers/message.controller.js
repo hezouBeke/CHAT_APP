883ebe83f63c96a93cb759242b7da399e8486322
hezou <hezoubeke18@gmail.com>
2025-05-16 11:59:32 +0000

    update

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,17 +1,15 @@
 import User from "../models/user.model.js";
 import Message from "../models/message.model.js";
 
-export const getUsersForSidebar = async (req, res) =>{
-
+export const getUsersForSidebar = async (req, res) => {
   try {
-    const LoggedInUserId = req.body.user._id;
-    const filteredUsers = await User.find({_id: {$ne: LoggedInUserId}}).select("-password");
+    const loggedInUserId = req.user._id;
+    const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
 
     res.status(200).json(filteredUsers);
-    
   } catch (error) {
-    console.log("error in getUsersForSidebar", error.message);
-    res.status(500).json({ error: "internal server error"}); 
+    console.error("Error in getUsersForSidebar: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
   }
 };
 
